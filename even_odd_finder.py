ccfb17f120abebc8bc0d72eb72484155f47fdbd2
lukemarak <lukemarak@gmail.com>
2017-12-02 14:13:57 +0530

    Add files via upload

---
+++ even_odd_finder.py
@@ -0,0 +1,8 @@
+for i in range(0, 10):
+    
+    '''if i % 2 == 0:
+        print("Even numbers: ", i)
+    else:
+        print("Odd numbers:  ", i)'''
+        
+    print("{} is even".format(i) if i % 2 == 0 else "{} is odd".format(i) )
